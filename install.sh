147d22cfd94ac4da272cad147ff54057335120c5
gioannou <dev.gioannou@gmail.com>
2024-01-08 20:25:02 +0200

    Add base install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+# ls replacement
+cargo install eza
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# trash-cli
+pip install trash-cli
+
+mv .tmux.conf ~/.tmux.conf
+
